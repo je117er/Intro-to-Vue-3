33835a313daa0f22eb9466c1a99636391c3d40a4
hangchu <hangchu@beeketing.net>
2022-01-15 15:25:55 +0700

    Add solution to L6-start

--- main.js
+++ main.js
@@ -11,5 +11,13 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        decrement() {
+            this.cart -= 1;
+        },
+        setVariantImage(variantImage) {
+            this.image = variantImage
+        }
     }
 })
